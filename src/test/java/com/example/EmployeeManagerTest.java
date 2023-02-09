dadb157348abe9aa91b3849c57ea9ebf1bf9377e
dzJP <113700179+dzJP@users.noreply.github.com>
2023-02-09 16:15:02 +0100

    Added testShouldFailIfAnyEmployeeHasSetPaidToFalse() in EmployeeManagerTest.java

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -2,8 +2,17 @@ package com.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.management.RuntimeErrorException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+@ExtendWith(MockitoExtension.class)
 public class EmployeeManagerTest {
 
     @Test
@@ -23,13 +32,13 @@ public class EmployeeManagerTest {
        }
 
     @Test
-    public void testReturnNotPaidShouldReturnOnlyNotPaid() {
+    public void testShouldFailIfAnyEmployeeHasSetPaidToFalse() {
         Employee employee1 = new Employee("1", 20000);
         employee1.setPaid(false); // change to true and test will pass
         Employee employee2 = new Employee("2",30000);
         employee2.setPaid(true);
 //       EmployeeManager employeeManager = new EmployeeManager(new EmployeeRepositoryStubReturnEmployees(), new DummyBankService());
 
-        Assertions.assertTrue(employee1.isPaid(),"Test will fail because employee1 is set to false");
+        Assertions.assertTrue(employee1.isPaid(),"Test will fail because employee1 has setPaid to false");
     }
 }
