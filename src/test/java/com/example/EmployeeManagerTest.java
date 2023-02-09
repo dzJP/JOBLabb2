c920211e022084f0cde3538cd1fa8e5e6d04d938
dzJP <113700179+dzJP@users.noreply.github.com>
2023-02-09 11:53:40 +0100

    Added setUpNewEmployee() in EmployeeManagerTest class and ran test successfully.

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,21 @@
 package com.example;
 
-public class EmployeeManagerTest {
+import org.junit.jupiter.api.Test;
 
+public class EmployeeManagerTest {
+    @Test
+    public void setUpNewEmployee() {
+        Employee employee1 = new Employee("1", 20000);
+        employee1.setId("1");
+        employee1.setSalary(20000);
+        employee1.setPaid(false);
+
+        Employee employee2 = new Employee("2", 30000);
+        employee2.setId("2");
+        employee2.setSalary(30000);
+        employee2.setPaid(true);
+
+        System.out.println("Running a test...");
+
+    }
 }
